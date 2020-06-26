f416ffb4c6020c60f989639fbdc8d6a2f1eaf501
Ashutosh2u2u <ashutoshgupta3006@gmail.com>
2020-06-26 10:48:52 +0530

    new code committed

--- UdemyPractice/src/test/java/javaPrograms/MinNumber.java
+++ UdemyPractice/src/test/java/javaPrograms/MinNumber.java
@@ -1,9 +1,21 @@
 package javaPrograms;
 
 public class MinNumber {
-
+//minimum number from multidimentional array
 	public static void main(String[] args) {
 		int abc[][]= {{2,4,5},{3,4,7},{1,2,9}};
+		int min=abc[0][0];//assuming abc[0][0] is minimum among the all
+		for(int i=0;i<3;i++) 
+		{			
+			for(int j=0;j<3;j++)
+			{
+				if(abc[i][j]<min)  //if any number less than assumed minimum value
+				{
+					min=abc[i][j];// replaced assumed min with actual min
+				}
+			}
+		}
+		System.out.println("min number is :" +min );
 	}
 
 }
